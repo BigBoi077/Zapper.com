40e9b64861404093e0f4515ded0b18cd149f91de
BigBoi077 <58580199+BigBoi077@users.noreply.github.com>
2021-04-19 17:35:37 -0400

    Made user model

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -1,10 +1,8 @@
 <?php namespace Models;
 
-use Guzzle\Stream\Stream;
-
 class Service
 {
     private int $id;
     private String $name;
-    private Stream $imagePath;
+    private String $imagePath;
 }
---
+++ app/Models/User.php
@@ -0,0 +1,11 @@
+<?php namespace Models;
+
+class User
+{
+    private String $firstname;
+    private String $lastname;
+    private String $username;
+    private String $email;
+    private String $phone;
+    private String $password;
+}
